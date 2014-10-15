052f66e9f5598f16cf329d54de0dfef1b58499bc
Bartosz Madry <bartosz.madry@o2.pl>
2014-10-15 14:26:28 +0200

    fix

--- src/main/java/com/logtomobile/zoomit/util/BundleConstants.java
+++
@@ -1,9 +0,0 @@
-package com.logtomobile.zoomit.util;
-
-/**
- * @author Bartosz Mądry
- */
-public class BundleConstants {
-    public static final int SPEED_READING_PER_MINUTE = 200;
-    public static final int TIME_FOR_SLEEP_ANSWER = 350;
-}
